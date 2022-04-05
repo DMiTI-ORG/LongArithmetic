9dde8b190864fe6ffe0fb05ab8410a061e95a0c1
nosnic <99653851+nosnic@users.noreply.github.com>
2022-04-05 17:21:41 +0300

    Add files via upload
    N-11 add

--- NaturalNumber.py
+++ NaturalNumber.py
@@ -39,31 +39,75 @@ class NaturalNumber:
         pass
 
     def subtract_k_by_number(self, number: Self, digit: int) -> Self:
-        """
-        module: SUB_NDN_N
-        author: Smirnov Nikita
+        # N-9
+        a = ""
+        b = ""
+        for i in self.array:
+            a += str(i)
+        for i in number.array:
+            b += str(i)
+        res = str(int(a) - int(b) * digit)
+        result = NaturalNumber(len(res), [])
+        for i in res:
+            result.array.append(int(i))
+        return result
 
-        arguments:
-            number: an instance of the class NaturalNumber
-            digit: one digit to multiply with number
-
-        This method subtract from self number another number multiplied with digit
-        """
-
-        new_num = number.multiply_digit(digit)
-        if self.compare(new_num) != 1:
-            return self.subtract(new_num)
-        else:
-            return 'Error'
-
-
-    def first_division_digit(self, number: Self, digit: int) -> Self:
+    def first_division_digit(self, number: Self) -> Self:
         # N-10
-        pass
+        a = ""
+        b = ""
+        for i in self.array:
+            a += str(i)
+        for i in number.array:
+            b += str(i)
+        res = str(int(a) // int(b))
+        result = NaturalNumber(len(res), [int(res[0])])
+        for i in range(len(res) - 1):
+            result.array.append(0)
+        return result
 
     def quotient(self, number: Self) -> Self:
-        # N-11
-        pass
+        """
+        module: <DIV_NN_N>
+        author: <Nickolay>
+
+        Arguments:
+            self: the dividend
+            number: the divider
+
+        This function is doing quotient of two natural numbers is calculated by repeatedly dividing
+        the first number by the second number and taking the remainder of each division
+        and appending it to the end of the quotient.
+        """
+        dividend = self
+        divider = number  # divider
+        result = NaturalNumber(0, [])  # result of quotient
+        current_position = divider.highest_position  # remember the length of the divisor
+        # take from the dividend a number equal in size to the divisor
+        current_dividend = NaturalNumber(current_position, dividend.array[:current_position])
+        # If suddenly the value of the taken number turned out to be less than the divisor,
+        # then we take 1 more digit from the dividend
+        if current_dividend.first_division_digit(divider).array[0] == 0:
+            current_dividend.array.append(dividend.array[current_position])
+            current_position += 1
+        # Actually division by a column
+        while current_position < dividend.highest_position:  # Until we reach the end of the number
+            # First, we take the first digit of the incomplete quotient from dividing the selected number by the divisor
+            quotient = current_dividend.first_division_digit(divider).array[0]
+            result.array.append(quotient)  # add the found number to the result
+            result.highest_position += 1  # increase the size of the resulting number by 1
+            # find the remainder of dividing the selected number by the divisor
+            remainder = current_dividend.subtract_k_by_number(divider, quotient)
+            # new dividend is got by adding the next digit from the dividend to remainder from the previous step
+            current_dividend = remainder
+            current_dividend.array.append(dividend.array[current_position])
+            current_dividend.highest_position = remainder.highest_position + 1
+            current_position += 1  # just move to the next position
+        # If suddenly we have already reached the end, then we make the last division of the new dividend by the divisor
+        if current_position == dividend.highest_position:
+            result.array.append(current_dividend.first_division_digit(divider).array[0])
+            result.highest_position += 1
+        return result
 
     def remainder(self, number: Self) -> Self:
         # N-12
@@ -71,15 +115,45 @@ class NaturalNumber:
 
     def gcd(self, number: Self) -> Self:
         # N-13
-        pass
-
+        a = ""
+        b = ""
+        for i in self.array:
+            a += str(i)
+        for i in number.array:
+            b += str(i)
+        a = int(a)
+        b = int(b)
+        while a != 0 and b != 0:
+            if a > b:
+                a = a % b
+            else:
+                b = b % a
+        c = str(a + b)
+        result = NaturalNumber(len(c), [])
+        for i in c:
+            result.array.append(int(i))
+        return result
 
     def lcm(self, number: Self) -> Self:
         # N-14
-        pass
-
-    def __eq__(self, other: Self) -> bool:
-        return (self.array == other.array) and (self.highest_position == other.highest_position)
+        a = ""
+        b = ""
+        c1 = self.gcd(number)
+        c = ""
+        for i in self.array:
+            a += str(i)
+        for i in number.array:
+            b += str(i)
+        for i in c1.array:
+            c += str(i)
+        a = int(a)
+        b = int(b)
+        c = int(c)
+        res = str(a * b // c)
+        result = NaturalNumber(len(res), [])
+        for i in res:
+            result.array.append(int(i))
+        return result
 
     def __str__(self) -> str:
         return ''.join(map(str, self.array))
--- TestNatural.py
+++ TestNatural.py
@@ -1,34 +1,31 @@
-import imp
+import unittest
 from unittest import TestCase
-from unittest.mock import patch
-
 from NaturalNumber import NaturalNumber
 
-
 class TestNatural(TestCase):
-    '''
+    """
     Example test
     @patch.object(Calculator, 'bar')
     def test_sum(self, bar):
         bar.return_value = 1
         self.assertEqual(self.calc.foo(100, 200), 1)
         bar.return_value = 5
-        self.assertEqual(self.calc.foo(100, 200), 5)'''
+        self.assertEqual(self.calc.foo(100, 200), 5)
+    """
 
-    @patch.object(NaturalNumber, 'multiply_digit')
-    @patch.object(NaturalNumber, 'compare')
-    @patch.object(NaturalNumber, 'subtract')
-    def test_subtract_k_by_number(self, subtract, compare, multiply_digit):
-        number_1 = NaturalNumber(2, [4, 1])
-        number_2 = NaturalNumber(2, [3, 0])
-        number_3 = NaturalNumber(2, [1, 1])
+    def test_quotient(self):
+        # 999 // 988 = 1
+        self.assertEqual(NaturalNumber(3, [9, 9, 9]).quotient(NaturalNumber(3, [9, 8, 8])).array,
+                         NaturalNumber(1, [1]).array)
+        # 10005 // 17 = 588
+        self.assertEqual(NaturalNumber(5, [1, 0, 0, 0, 5]).quotient(NaturalNumber(2, [1, 7])).array,
+                         NaturalNumber(3, [5, 8, 8]).array)
+        # 9678 // 9678 = 1
+        self.assertEqual(NaturalNumber(4, [9, 6, 7, 8]).quotient(NaturalNumber(4, [9, 6, 7, 8])).array,
+                         NaturalNumber(1, [1]).array)
 
-        compare.return_value = 2
-        subtract.return_value = NaturalNumber(2, [1, 1])
-        multiply_digit.return_value = NaturalNumber(2, [3, 0])
-        self.assertEqual(str(number_3), str(number_1.subtract_k_by_number(number_2, 1)))
 
-        multiply_digit.return_value = NaturalNumber(1, [5])
-        compare.return_value = 1
-        subtract.return_value = NaturalNumber(1, [-1])
-        self.assertEqual(number_1.subtract_k_by_number(number_2, 1), 'Error')
+
+
+if __name__ == '__main__':
+    unittest.main()
