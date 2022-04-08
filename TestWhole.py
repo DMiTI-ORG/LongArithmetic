ece3820f3fbd562a7d3d8e2f3d308f1d3072fb3b
Банит Максим <tsusahi@bk.ru>
2022-04-08 20:04:07 +0300

    Complete Z-1, Z-2

--- TestWhole.py
+++ TestWhole.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from unittest.mock import patch
+from NaturalNumber import NaturalNumber
+from WholeNumber import WholeNumber
 
 class TestWhole(TestCase):
     '''
@@ -10,3 +12,22 @@ class TestWhole(TestCase):
         self.assertEqual(self.calc.foo(100, 200), 1)
         bar.return_value = 5
         self.assertEqual(self.calc.foo(100, 200), 5)'''
+
+    def test_is_positive(self):
+        res_1 = 1 #negative
+        res_2 = 2 #positive
+        res_3 = 0 #zero
+
+        num_1 = WholeNumber(1, 2, [1, 0]) #negative
+        num_2 = WholeNumber(0, 2, [3, 3]) #positive
+        num_3 = WholeNumber(1, 2, [0, 0]) #zero
+        num_4 = WholeNumber(0, 2, [0, 0]) #zero
+
+        self.assertEquals(res_1, num_1.is_positive())
+
+    def test_abc(self):
+        res = NaturalNumber(3, [1, 2, 3])
+
+        num = WholeNumber(1, 3, [1, 2, 3])
+
+        self.assertEqual(res, num.abs())
